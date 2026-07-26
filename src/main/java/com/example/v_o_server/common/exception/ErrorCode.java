c405b1ed0a4f6e7ce9aff205b6ade916eeb3646d
micha <michaelseokju@naver.com>
2026-07-26 16:26:05 +0900

    fix: ErrorCode 수정

--- src/main/java/com/example/v_o_server/common/exception/ErrorCode.java
+++ src/main/java/com/example/v_o_server/common/exception/ErrorCode.java
@@ -54,7 +54,7 @@ public enum ErrorCode {
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "V003", "댓글을 찾을 수 없습니다."),
     COMMENT_NO_PERMISSION(HttpStatus.FORBIDDEN, "V004", "댓글 작성자만 수정/삭제할 수 있습니다."),
     COMMENT_TOO_LONG(HttpStatus.BAD_REQUEST, "V005", "댓글은 최대 100자까지 입력 가능합니다."),
-    COMMENT_BLANK(HttpStatus.BAD_REQUEST, "V006", "댓글 내용을 입력해주세요.");
+    COMMENT_BLANK(HttpStatus.BAD_REQUEST, "V006", "댓글 내용을 입력해주세요."),
     // 그룹
     GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "G001", "그룹을 찾을 수 없습니다."),
     GROUP_NAME_DUPLICATED(HttpStatus.CONFLICT, "G002", "이미 사용 중인 그룹명입니다."),
