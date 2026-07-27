63de72e7f1c7c672134df3b7729f3c260ac7f80d
micha <michaelseokju@naver.com>
2026-07-27 20:41:40 +0900

    fix : ErrorCode, VOServerApplication 파일 수정

--- src/main/java/com/example/v_o_server/VOServerApplication.java
+++ src/main/java/com/example/v_o_server/VOServerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
-@ConfigurationPropertiesScan
 public class VOServerApplication {
 
     public static void main(String[] args) {
